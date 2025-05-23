d0a8e4ba95e6b9b15eba2290c9f324463981ebc0
KornchawanCS801 <Kornchawan.pasi@mail.kmutt.ac.th>
2025-05-23 21:06:03 +0700

    Updated responsive addFoodPost fix nav

--- Frontend/src/components/NavbarNoSearchingBar.jsx
+++ Frontend/src/components/NavbarNoSearchingBar.jsx
@@ -10,7 +10,7 @@ const NavbarNoSearchingBar = () => {
                 <NavLink to={"/home"} className='max-sm:text-[20px] text-[36px] font-bold ml-2 lg:ml-10 cursor-pointer'>
                     <h1 className='flex flex-row'>
                         <span className='text-[#DE0000]'>Abb&nbsp;</span>
-                        <span className='text-[#000000]'>Sapp</span>
+                        <span className='text-[#000000]'>Sabb</span>
                     </h1>
                 </NavLink>
                 <NavLink to={"/login"} className='flex items-center gap-3 border-1 border-[#D9D9D9] py-1 px-1 lg:py-2 lg:px-6 rounded-2xl ml-2 mr-1 lg:mr-10 hover:border-black cursor-pointer'>
