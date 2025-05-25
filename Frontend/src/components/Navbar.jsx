0b60dc1f53c3f224469069f971124e51cd279fd9
KornchawanCS801 <Kornchawan.pasi@mail.kmutt.ac.th>
2025-05-25 14:02:36 +0700

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -46,9 +46,9 @@ const Navbar = () => {
             </NavLink>
             {!login &&
             <>
-            <NavLink to={"/login"} className='flex items-center gap-3 border-1 border-[#D9D9D9] py-2 px-6 rounded-2xl mr-10 hover:border-black cursor-pointer'>
-                <FaUser className='border-1 rounded-[100%] text-[48px] p-1'/>
-                <p className='text-[24px] font-bold'>Login</p>
+            <NavLink to={"/login"} className='flex items-center gap-3 border-1 border-[#D9D9D9] py-1 px-1 lg:py-2 lg:px-6 rounded-2xl ml-2 mr-1 lg:mr-10 hover:border-black cursor-pointer'>
+                <FaUser className='border-1 rounded-[100%] max-sm:text-[30px] text-[48px] p-1'/>
+                <p className='text-[20px] lg:text-[24px] font-bold'>Login</p>
             </NavLink>
             </>
             }
@@ -56,7 +56,7 @@ const Navbar = () => {
             <>
             {!dropDown &&
             <div onClick={() => setDropDown(true)} className='flex items-center gap-3 border-1 border-[#D9D9D9] py-2 px-6 rounded-2xl mr-10 hover:border-black cursor-pointer'>
-                <FaUser className='border-1 rounded-[100%] text-[48px] p-1'/>
+                <FaUser className='border-1 rounded-[100%] p-1 max-sm:text-[30px] text-[48px]'/>
                 <p className='text-[24px] font-bold'>{username}</p>
             </div>
             }
